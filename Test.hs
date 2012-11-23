53571caceeee7b3c2d9bf46f4162b10dc58961d8
Fai <evilinfuture@gmail.com>
2012-11-23 18:19:52 +0400

    more complex test

--- Test.hs
+++ Test.hs
@@ -3,6 +3,15 @@ module Test where
 import Piece
 import Board
 
-board = addPiece ('a',1) (Piece White Rook) emptyBoard
+board = 
+      addPiece ('a',1) (Piece White Rook) 
+	$ addPiece ('b',1) (Piece White Knight) 
+	$ addPiece ('c',1) (Piece White Bishop) 
+	$ addPiece ('d',1) (Piece White Queen) 
+	$ addPiece ('e',1) (Piece White King) 
+	$ addPiece ('f',1) (Piece White Bishop)
+	$ addPiece ('g',1) (Piece White Knight) 
+	$ addPiece ('h',1) (Piece White Rook) 
+	emptyBoard
 
 main = putStrLn $ displayBoard board
