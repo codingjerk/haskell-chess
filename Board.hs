3ad8d4b4da0cc8414247c006378c8842f583de6e
Coding Jerk <codingjerk@gmail.com>
2018-01-17 08:38:59 +0400

    setSquare :: Position -> Square -> Board -> Board

--- Board.hs
+++ Board.hs
@@ -1,11 +1,12 @@
 module Board(
-	Position,
+	Position(..),
 	Square(..),
-	Board(..),
+	Board,
 	emptyBoard,
 	displayBoard,
 	addPiece,
-	removePiece
+	removePiece,
+	setSquare
 ) where
 
 import Piece
@@ -38,13 +39,16 @@ emptyBoard = array boardRanges $ map (\x -> (x, Empty)) $ range boardRanges
 
 addPiece :: Position -> Piece -> Board -> Board
 addPiece pos piece board 
-	| board ! pos == Empty = board // [(pos, Occupied piece)]
+	| board ! pos == Empty = setSquare pos (Occupied piece) board
 	| otherwise = error "Trying to add piece on non empty square"
 
 removePiece :: Position -> Board -> Board
 removePiece pos board
 	| board ! pos == Empty = error "Trying to remove piece from empty square"
-	| otherwise = board // [(pos, Empty)]
+	| otherwise = setSquare pos Empty board
+
+setSquare :: Position -> Square -> Board -> Board
+setSquare pos square board = board // [(pos, square)]
 
 displayBoard :: Board -> String
 displayBoard b = concatMap displayLine $ reverse [fst yranges .. snd yranges] where
