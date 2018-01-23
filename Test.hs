7ee74be5c20354fc9abb0516f7aa0b0660bc5e02
Coding Jerk <codingjerk@gmail.com>
2018-01-23 13:54:45 +0400

    more tests

--- Test.hs
+++ Test.hs
@@ -3,7 +3,7 @@ module Test where
 import Piece
 import Board
 
-board = 
+testingBoard1 = 
       addPiece ('a',1) (Piece White Rook) 
 	$ addPiece ('b',1) (Piece White Knight) 
 	$ addPiece ('c',1) (Piece White Bishop) 
@@ -14,6 +14,12 @@ board =
 	$ addPiece ('h',1) (Piece White Rook) 
 	emptyBoard
 
-main = do
-	putStrLn $ displayBoard $ newBoard
-	putStr $ displayBoard $ boardFromFen "4k3/8/4K3/8/8/8/R7/8"
+testingBoard2 = boardFromFen "4k3/8/4K3/8/8/8/R7/8"
+
+testingBoard3 = newBoard
+
+test b = do
+	putStr $ displayBoard b
+	putStrLn $ (boardToFen b) ++ "\n"
+
+main = mapM_ (test) [testingBoard1, testingBoard2, testingBoard3]
