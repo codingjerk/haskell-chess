98deee9d7387063d42b96b46718c0f5ec50d1e52
Coding Jerk <codingjerk@gmail.com>
2018-02-05 17:27:12 +0400

    tests for makeMoveLow

--- Test.hs
+++ Test.hs
@@ -1,16 +1,14 @@
 module Test where
 
 import Position
+import Move
 
 fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 position = positionFromFen fen
 
+e2e4_position = makeMoveLow (Move NormalMove ('e',2) ('e',4)) position
+
 main = do 
-	putStrLn $ displayPosition position
-	putStrLn "\n---\n"
-
-	putStrLn $ positionToFen position
-	putStrLn "\n---\n"
-
-	print $ fen == positionToFen position
+	putStrLn $ displayPosition position ++ "\n\n"
+	putStrLn $ displayPosition e2e4_position
