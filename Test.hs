a23193e21ad137195641e3264f7811ebe7e2c045
Coding Jerk <codingjerk@gmail.com>
2018-03-21 22:52:32 +0400

    module Test refactoring

--- Test.hs
+++ Test.hs
@@ -1,16 +1,19 @@
---module Test where
+module Test (
+	test
+) where
 
 import System.Environment
 import Position
 import Perft
-import Move
-import Generator
 
+fen :: String
 fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
+position :: Position
 position = positionFromFen fen
 
-main = do
+test :: IO ()
+test = do
 	args <- getArgs
 	putStrLn $ "Число узлов для глубины " ++ depth args ++ ":"
 	print $ perft (read $ depth args) position where
