97f7437fab9bffdac83f91a42975af02333d9d71
Coding Jerk <codingjerk@gmail.com>
2018-03-21 22:52:12 +0400

    perft refactoring

--- Perft.hs
+++ Perft.hs
@@ -8,6 +8,7 @@ import Generator
 perft :: Int -> Position -> Int
 perft depth pos 
 	| depth <= 0 = 1
-	| otherwise  = sum $ map (\nextpos -> perft (depth - 1) nextpos) $ legalPositions where
+	| otherwise  = sum leafs where
+		leafs = map (perft $ depth - 1) $ legalPositions
 		legalPositions = filter isLegalPosition nextpositions
-		nextpositions = map (\move -> makeMove move pos) $ (moves pos)
+		nextpositions = map (flip makeMove pos) $ moves pos
