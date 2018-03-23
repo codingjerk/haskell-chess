ca5527d11645c92c4c21fc8bff4a0e1646ebe252
Coding Jerk <codingjerk@gmail.com>
2018-03-23 14:48:16 +0400

    haddock documentation for module Board

--- Board.hs
+++ Board.hs
@@ -1,14 +1,28 @@
+-- | The Board module is responsible for 
+-- providing squares, boards, 
+-- transfering them to a string, 
+-- as well functions to add and remove shapes.
+
 module Board(
+    -- * Types
     Square(..),
-    Board,
+    Board(..),
+
+    -- * Common Boards
     emptyBoard,
-    displayBoard,
+    initialBoard,
+
+    -- * Board's base functions
     addPiece,
     removePiece,
     setSquare,
-    initialBoard,
+
+    -- * Board's representation functions
+    displayBoard,
     boardToFen,
     boardFromFen,
+
+    -- * Size of Board
     xranges,
     yranges
 ) where
@@ -21,6 +35,15 @@ import Data.Array
 import Data.List
 import Data.Char
 
+-- | Square is a synonim to Maybe Piece.
+-- 
+-- It may adopt a two values: 
+-- 
+--     * Nothing for empty square.
+-- 
+--     * Just 'Piece' for piece on square.
+-- 
+-- > Just (Piece White King)
 type Square = Maybe Piece
 
 squareWidth :: Int
@@ -37,30 +60,56 @@ squareAsFen :: Square -> String
 squareAsFen Nothing = "."
 squareAsFen (Just p) = pieceToFen p
 
+-- | Board is array of Squares
+-- where index of this array is coordinate ('Coord').
 type Board = Array Coord Square
 
+-- | This value contains a bounds of 'Board' by x-axis.
+-- 
+-- by default it is equal to @(\'a\', \'h\')@
 xranges :: (Char, Char)
 xranges = ('a', 'h')
 
+-- | This value contains a bounds of 'Board' by y-axis.
+-- 
+-- by default it is equal to @(1, 8)@
 yranges :: (Int, Int)
 yranges = (1, 8)
 
 boardRanges :: (Coord, Coord)
 boardRanges = ( (fst xranges, fst yranges), (snd xranges, snd yranges) )
 
+-- | 'emptyBoard' represent a blank board with no 'Piece's.
+-- 
+-- In other case emptyBoard is array of empty 'Square's.
 emptyBoard :: Board
 emptyBoard = array boardRanges $ map (\x -> (x, Nothing)) $ range boardRanges
 
+-- | 'addPiece' gets a 'Piece' to set, 'Coord'inates and a 'Board' to set on
+-- and returns a new 'Board' with a new setted 'Piece'.
+-- 
+-- This function may generate error, if you try to add 'Piece' over
+-- another 'Piece'.
 addPiece :: Coord -> Piece -> Board -> Board
 addPiece pos piece board 
     | board ! pos == Nothing = setSquare pos (Just piece) board
     | otherwise = error "Trying to add piece on non Nothing square"
 
+-- | 'removePiece' gets a 'Coord'inates and a 'Board' to remove piece
+-- and returns a new 'Board' without removed 'Piece'.
+--
+-- This function may generate error, if you try to remove 'Piece' from
+-- empty 'Square'.
 removePiece :: Coord -> Board -> Board
 removePiece pos board
     | board ! pos == Nothing = error "Trying to remove piece from Nothing square"
     | otherwise = setSquare pos Nothing board
 
+-- | 'setSquare' gets a 'Coord'inates, 'Square' and a 'Board' to change
+-- and returns a new 'Board' with changed 'Square'.
+--
+-- This function didn't generate erorrs and may be used then we don't
+-- care that contain prevous Square.
 setSquare :: Coord -> Square -> Board -> Board
 setSquare pos square board = board // [(pos, square)]
 
@@ -72,6 +121,8 @@ boardToString :: (Square -> String) -> String -> String -> Board -> String
 boardToString f squareSep lineSep b = intercalate lineSep $ map lineToString $ reverse [fst yranges .. snd yranges] where
     lineToString y = intercalate squareSep $ map (\x -> f $ b ! (x, y)) $ [fst xranges .. snd xranges]
 
+-- | This function gets a 'Board' and returns it's String implementation
+-- in FEN-notation.
 boardToFen :: Board -> String
 boardToFen b = concat $ replaceDots $ group $ boardToString squareAsFen "" "/" b where
     replaceDots = map (\a -> if (elem '.' a) then show (length a) else a)
@@ -80,6 +131,10 @@ splitOn :: Eq a => a -> [a] -> [[a]]
 splitOn _ [] = []
 splitOn e xs = takeWhile (/=e) xs: splitOn e (drop 1 $ dropWhile (/=e) xs)
 
+-- | 'boardFromFen' gets a fen-string and returns a 'Board' for 
+-- this record.
+--
+-- This function may generate error if fen is not valid. 
 boardFromFen :: String -> Board
 boardFromFen fen = fromList $ map (createLine) splitedFen where
     createLine [] = []
@@ -96,9 +151,12 @@ boardFromFen fen = fromList $ map (createLine) splitedFen where
                     transposedsqs = concat $ map reverse $ transpose sqs
                     indexes = range boardRanges
 
+-- | 'displayBoard' gets a 'Board' and returns it's String implementation
+-- in cute view.
 displayBoard :: Board -> String
 displayBoard b = boardToString displaySquare "" "\n" b ++ "\n"
 
+-- | 'initialBoard' returns a board with standart starting position in chess.
 initialBoard :: Board
 initialBoard = array boardRanges $ map (\x -> (x, piece x)) $ range boardRanges where
     white = Just . Piece White
