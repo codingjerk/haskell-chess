ecc5bc9927034b52ea476f0ecf1fc8ced214944e
Coding Jerk <codingjerk@gmail.com>
2018-03-27 15:20:40 +0400

    Move documentation

--- Move.hs
+++ Move.hs
@@ -1,25 +1,45 @@
+-- | Move module contains base type 'Move' and 
+-- function to convert it in humanity-string.
+
 module Move(
+    -- * Types
     Move(..),
     MoveType(..),
+
+    -- * Display-functions
     displayMove
 ) where
 
 import Coord
 import Piece
 
+-- | This type contains kinds of moves.
 data MoveType = 
+    -- | NormalMove implements a non-capture common move.
     NormalMove | 
+    -- | CaptureMove implements a common caprute.
     CaptureMove |
 
+    -- | PromotionMove is 'Pawn'-specific move, that contains a type of piece to promote in.
     PromotionMove PieceType |
+    -- | PawnCapture is 'Pawn'-specific move, that implements a capture move of 'Pawn'.
     PawnCapture |
+    -- | PawnDoubleMove is move that can make 'Pawn' from start position.
     PawnDoubleMove |
+    -- | Enpassant is a special chess move, that also is a capture.
     EnpassantMove | 
 
+    -- | Long Castling (to 'Queen' side) is special 'King' move, that also affects 'Rook' on a-file 
     LongCastlingMove |
+    -- | Short Castling (to 'King' side) is special 'King' move, that also affects 'Rook' on h-file 
     ShortCastlingMove 
     deriving (Show, Eq)
 
+-- | Move type contains from, to coordinates and a type of move.
+-- 
+-- So, this is example of Move value:
+--
+-- > Move NormalMove ('b',3) ('f',3)
 data Move = Move {
     kind :: MoveType,
     from :: Coord, 
@@ -27,6 +47,11 @@ data Move = Move {
 
 } deriving (Show, Eq)
 
+-- | displayMove function gets a move and returns it's string representation.
+-- 
+-- As example:
+-- 
+-- > displayMove (Move CaptureMove ('e',2) ('e',6)) == "e2xe6"
 displayMove :: Move -> String
 displayMove (Move NormalMove f t) = coordToFen f ++ "-" ++ coordToFen t
 displayMove (Move CaptureMove f t) = coordToFen f ++ "x" ++ coordToFen t
