62eae6c692e6f341e0ce6a61b7972b4182caec2b
Coding Jerk <codingjerk@gmail.com>
2018-03-28 15:29:36 +0400

    Perft documentation

--- Perft.hs
+++ Perft.hs
@@ -1,3 +1,5 @@
+-- | Module Perft contains a testing only one testing function 'perft'.
+
 module Perft(
     perft
 ) where
@@ -5,6 +7,10 @@ module Perft(
 import Position
 import Generator
 
+-- | Perft is an alogithm that calculate nodes count for some position
+-- on some depth. This function gets a depth, position and returns nodes count.
+--
+-- This function is very usefull for testing and debuging generator, 'Board' and 'Position'.
 perft :: Int -> Position -> Int
 perft depth pos 
     | depth <= 0 = 1
