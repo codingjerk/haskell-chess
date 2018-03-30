30a484bd614c0bba44e4c1bf4ba559895c205475
Coding Jerk <codingjerk@gmail.com>
2018-03-30 15:49:46 +0400

    Test doc-on

--- Test.hs
+++ Test.hs
@@ -1,3 +1,6 @@
+-- | Module Test created for testing all other modules, 
+-- and will be main module, until development done.
+
 module Test (
     test
 ) where
@@ -12,6 +15,8 @@ fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 position :: Position
 position = positionFromFen fen
 
+-- | test function gets nothing, but doing IO-actions and
+-- printing result of testig in terminal.
 test :: IO ()
 test = do
     args <- getArgs
